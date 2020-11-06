9762f1c352919d39a923b5e14fed3f0bbd6f4909
lvthinh100 <73948433+lvthinh100@users.noreply.github.com>
2020-11-06 22:45:27 +0700

    Tao File moi

---
+++ File-test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+bool isEven(int n)
+{
+	if (n==1) return false;
+	return !isEven(n-1);
+}
+
+int main(){
+	int a;
+	cin >> a;
+	if(isEven(a)) cout<<a<<" la so chan";
+	else cout<<a<<" la so le";
+	return 0;
+	
+}
